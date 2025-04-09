d3f3f06bac89e4ebab28d7af45230f2901317135
Lozynskyi Nazarii <nazarii.lozynskyi@lantec.ua>
2025-04-09 20:42:01 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all interfaces
+    port: 5173,      // Default port (optional, already 5173 by default)
+  },
+});
